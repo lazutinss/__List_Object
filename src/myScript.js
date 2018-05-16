3e93f241355694e424adc7d8d1e48f126165b69e
yury.soiko <yury.soiko@vrpconsulting.com>
2018-05-16 10:01:27 +0300

    made tab panel, Multi select pick list

--- src/myScript.js
+++ src/myScript.js
@@ -1,5 +1,33 @@
+$(document).ready(function(){
+    alert(jQuery.fn.jquery);
+});
+j$ = jQuery.noConflict();
+j$('.accountDiv').hide();
+
 function showFieldList(name){
     //console.log(name);
     showFields(name);
     return false;
+}
+//display active tab
+// var activeTab = event.target.getAttribute("id");
+// $(".gpSessionTab").removeClass("slds-is-active");
+// $("#"+activeTab).parent().addClass("slds-is-active");
+//
+// $(".gpSessionTabScope").each(function(){
+//     var clsName = $(this).attr("class");
+//     if(clsName.indexOf("slds-show") != -1){
+//         $(this).removeClass("slds-show");
+//         $(this).addClass("slds-hide");
+//     }
+// });
+// var arr = activeTab.split("__");
+// var scopeId = arr[0];
+// $("#"+scopeId).removeClass("slds-hide");
+// $("#"+scopeId).addClass("slds-show");
+//
+// }
+
+function changeTab(){
+   alert('sedfwqd');
 }
